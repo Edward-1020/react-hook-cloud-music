977344881bd5c79c89a0f72f8463789687c28719
Edward-1020 <iezzuyoung@gmail.com>
2020-01-26 22:51:30 +0800

    feat: recommend

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,29 +1,50 @@
 import React from 'react';
 import Slider from '../../components/slider';
+import { connect } from 'react-redux';
+import * as actionTypes from './store/actionCreators';
 import RecommendList from '../../components/list';
+import Scroll from '../../baseUI/scroll/index';
+import { Content } from './style';
 
 function Recommend () {
-  const bannerList = [1, 2, 3, 4].map(item => {
-    return {
-      imageUrl: 'http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg'
-    }
-  });
+  const { bannerList, recommendList } = props;
 
-  const recommendList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(item => {
-    return {
-      id: 1,
-      picUrl: "https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg",
-      playCount: 17171122,
-      name: "朴树、许巍、李健、郑钧、老狼、赵雷"
-    }
-  })
+  const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
+
+  useEffect(() => {
+    getBannerDataDispatch();
+    getRecommendListDataDispatch();
+  }, []);
+
+  const bannerListJS = bannerList ? bannerList.toJS() : [];
+  const recommendListJS = recommendList ? recommendList.toJS() : [];
 
   return (
-    <div>
-      <Slider bannerList={bannerList}></Slider>
-      <RecommendList recommendList={recommendList}></RecommendList>
-    </div>
+    <Content>
+      <Scroll>
+        <div>
+          <Slider bannerList={bannerList}></Slider>
+          <RecommendList recommendList={recommendList}></RecommendList>
+        </div>
+      </Scroll>
+    </Content>
   )
 }
 
-export default React.memo(Recommend);
+const mapStateToProps = (state) => ({
+  bannerList: state.getIn(['recommend', 'bannerList']),
+  recommendList: state.getIn(['recommend', 'recommendList'])
+})
+
+const mapDispathToProps = (dispath) => {
+  return {
+    getBannerDataDispatch () {
+      dispath(actionTypes.getBannerList());
+    },
+    getRecommendListDataDispath () {
+      dispath(actionTypes.getRecommendList());
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispathToProps)(React.memo(Recommend));
